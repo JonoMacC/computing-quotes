f650f1d1996c6a61d3695c4158256f34122c3ae3
JonoMacC <39803935+JonoMacC@users.noreply.github.com>
2020-07-09 16:55:09 -0700

    Update api routes and netlify redirects

--- src/lambda/api.js
+++ src/lambda/api.js
@@ -9,15 +9,16 @@ import serverless from "serverless-http";
 const app = express();
 const router = express.Router();
 
-// Import and mount the authorsRouter
-// const authorsRouter = require("../api/authors.js");
-// app.use("/api/authors", authorsRouter);
+// Import the authorsRouter
+const authorsRouter = require("../api/authors.js");
 
-// Import and mount the quotesRouter
-// const quotesRouter = require("../api/quotes.js");
+// Import the quotesRouter
+const quotesRouter = require("../api/quotes.js");
 
 // gzip responses
 router.use(compression());
+authorsRouter.use(compression());
+quotesRouter.use(compression());
 
 router.get("/", function (req, res) {
   res.send("hello world");
@@ -27,12 +28,22 @@ router.get("/", function (req, res) {
 app.use(morgan("dev"));
 
 // Setup routes
-app.use("/.netlify/functions/api/", router);
+app.use("/.netlify/functions/api/authors", authorsRouter);
+app.use("/.netlify/functions/api/quotes", quotesRouter);
+// app.use("./netlify/functions/api", router);
 
 // Mount express middleware
 router.use(cors());
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({ extended: true }));
+
+authorsRouter.use(cors());
+authorsRouter.use(bodyParser.json());
+authorsRouter.use(bodyParser.urlencoded({ extended: true }));
+
+quotesRouter.use(cors());
+quotesRouter.use(bodyParser.json());
+quotesRouter.use(bodyParser.urlencoded({ extended: true }));
 
 // module.exports = app;
 exports.handler = serverless(app);
